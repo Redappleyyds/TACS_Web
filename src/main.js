5f2a88039ff3914f974eebc8cdb148a0e415f966
Redappleyyds <3134641406@qq.com>
2024-10-20 00:12:24 +0800

    修复了一些BUG

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
-//移动端菜单
+// 移动端菜单
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('input.navbar-menu');
     const navbarMenuBlock = document.querySelector('.navbar-menu\\:block');
+    const mobileNavbar = document.querySelector('.mobile-navbar');
 
     const updateMenuDisplay = () => {
-        if (menuToggle.checked) {
-            navbarMenuBlock.classList.add('visible'); // 添加类以设置目标高度和透明度
-        } else {
-            navbarMenuBlock.classList.remove('visible'); // 移除类以重置
+        // 获取.mobile-navbar当前的display值
+        const mobileNavbarDisplay = window.getComputedStyle(mobileNavbar).display;
+
+        // 仅当.mobile-navbar的display为block时执行
+        if (mobileNavbarDisplay === 'block') {
+            if (menuToggle.checked) {
+                navbarMenuBlock.classList.add('visible'); // 添加类以设置目标高度和透明度
+            } else {
+                navbarMenuBlock.classList.remove('visible'); // 移除类以重置
+            }
         }
     };
 
